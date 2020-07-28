1dddd15caf6936aefe68038e7b1e4b75f968e19a
liuzp <toliuzp@gmail.com>
2020-07-28 20:57:05 +0800

    Add.

---
+++ tool/read_files.py
@@ -0,0 +1,17 @@
+
+import os
+ 
+f = open("./all_hdf5_file_list.txt",'w') #先创建一个空的文本
+path ="././indoor3d_ins_seg_hdf5"    #指定需要读取文件的目录
+files =os.listdir(path) #采用listdir来读取所有文件
+files.sort() #排序
+s= []                   #创建一个空列表
+for file_ in files:     #循环读取每个文件名
+#    print(path +file_)
+    if not  os.path.isdir(path +file_):  #判断该文件是否是一个文件夹
+        f_name = str(file_)
+#        print(f_name)
+        s.append(f_name)  # 把当前文件名返加到列表里
+        f.write(f_name + '\n') # 写入之前的文本中
+ 
+print(s) #看一下列表里的内容
